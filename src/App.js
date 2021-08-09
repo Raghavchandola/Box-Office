1877ff0b73e6e69dd599b6b0e8f2009eff15e061
Raghavchandola <raghavchandola@gmail.com>
2021-08-09 14:52:34 +0530

    Created page components and still has lots of errors

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import Nav from './components/Nav';
+import Home from './pages/Home';
+import Starred from './pages/Starred';
 
 function App() {
   return (
-    <Switch>
-      <Route exact path="/">
-        This is home page
-      </Route>
+    <div>
+      <Nav />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
 
-      <Route exact path="/starred">
-        This is starred
-      </Route>
+        <Route exact path="/starred">
+          <Starred />
+        </Route>
 
-      <Route>This is 404 page</Route>
-    </Switch>
+        <Route>This is 404 page</Route>
+      </Switch>
+    </div>
   );
 }
 
