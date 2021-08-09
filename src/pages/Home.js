05939b2601b934dc1c740f8afd4a9a22ecb8f264
Raghavchandola <raghavchandola@gmail.com>
2021-08-10 00:06:01 +0530

    displayed results and reworked api

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,24 +1,43 @@
 import React, { useState } from 'react';
 import MainPageLayout from '../components/MainPageLayout';
+import { apiGet } from '../misc/config';
 
 const Home = () => {
   const [input, setInput] = useState('');
-
+  const [results, setResults] = useState(null);
   const onInputChange = ev => {
     setInput(ev.target.value);
     console.log(ev.target.value);
   };
+
   const onSearch = () => {
-    // https://api.tvmaze.com/search/shows?q=girls
-    fetch(`https://api.tvmaze.com/search/shows?q=${input}`)
-      .then(r => r.json())
-      .then(result => console.log(result));
+    // /search/shows?q=girls
+    apiGet(`/search/shows?q=${input}`).then(result => {
+      setResults(result);
+      console.log(result);
+    });
   };
 
   const onKDown = ev => {
     if (ev.keyCode === 13) {
       onSearch();
     }
+  };
+
+  const renderResults = () => {
+    if (results && results.length === 0) {
+      return <div>No Results</div>;
+    }
+    if (results && results.length > 0) {
+      return (
+        <div>
+          {results.map(item => (
+            <div key={item.show.id}> {item.show.name} </div>
+          ))}
+        </div>
+      );
+    }
+    return null;
   };
 
   return (
@@ -32,6 +51,7 @@ const Home = () => {
       <button type="button" onClick={onSearch}>
         Search
       </button>
+      {renderResults()}
     </MainPageLayout>
   );
 };
