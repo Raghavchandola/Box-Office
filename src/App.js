bad7fae2cad6edf61f33d4848d530becd57b07ae
Raghavchandola <raghavchandola@gmail.com>
2021-09-16 13:30:12 +0530

    added and edited show page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-// import Nav from './components/Nav';
+
 import Home from './pages/Home';
 import Starred from './pages/Starred';
+import Show from './pages/Show';
 
 function App() {
   return (
-    <div>
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
+    <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
 
-        <Route exact path="/starred">
-          <Starred />
-        </Route>
+      <Route exact path="/starred">
+        <Starred />
+      </Route>
 
-        <Route>This is 404 page</Route>
-      </Switch>
-    </div>
+      <Route exact path="/show/:id">
+        <Show />
+      </Route>
+
+      <Route>
+        <div>Not found</div>
+      </Route>
+    </Switch>
   );
 }
 
