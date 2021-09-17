b508dc5379c19147884c5a75bebd77ef84cb554f
Raghavchandola <raghavchandola@gmail.com>
2021-09-17 14:36:48 +0530

    Updates

---
+++ precache-manifest.3bf5d5ead601d3808496c2b9be03e301.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e9c04cfe0effcfed41f2baa703fa165",
+    "url": "/Box-Office/index.html"
+  },
+  {
+    "revision": "7108014b00ad45d45290",
+    "url": "/Box-Office/static/css/main.93052401.chunk.css"
+  },
+  {
+    "revision": "312f5b99587965018efd",
+    "url": "/Box-Office/static/js/2.f5834b30.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Box-Office/static/js/2.f5834b30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7108014b00ad45d45290",
+    "url": "/Box-Office/static/js/main.56fb1390.chunk.js"
+  },
+  {
+    "revision": "6740a4f854f98d4fa166",
+    "url": "/Box-Office/static/js/runtime-main.140a3920.js"
+  }
+]);
